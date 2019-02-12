e7363cdb5e34dc4a03f890913b0c16e888e2fd17
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-12 16:01:58 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ require "pry"
 
 class Hash
   def keys_of(*arguments)
-   select { |key, value| if arguments.include?(value)}.keys
+   select { |key, value| if arguments.include? value}.keys
       binding.pry
 end
 end
